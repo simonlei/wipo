7ba935f4685e45dfd1b0d39ad14820970a3565d4
simonlei <simonlei@36a1d1b6-861c-0410-8dcb-f754f489a112>
2006-09-03 15:02:54 +0000

    Can create page

--- trunk/app/controllers/main_controller.rb
+++ trunk/app/controllers/main_controller.rb
@@ -1,6 +1,13 @@
 class MainController < ApplicationController
+  helper :calendar
+
+  def block (d)
+  end 
 
   def welcome
     @spaces = Space.find(:all)
+    @year = params[:year] || 2006
+    @month = params[:month] || 9
+    @weblogs = Page.find :all, :limit=>20, :order => 'created_at'
   end
 end
--- trunk/app/controllers/page_controller.rb
+++ trunk/app/controllers/page_controller.rb
@@ -1,6 +1,26 @@
 class PageController < ApplicationController
+  scaffold :page
 
   def show
     @page = Page.find( params[:id])
   end
+
+  def new
+    @page = Page.new
+    @page.space_id = params[:space_id]
+#    @page.space = Space.find( params[:space_id])
+    render_scaffold
+  end
+
+  def create
+    @page = Page.new(params[:page])
+#    @page.space_id = params[:space_id]
+    if @page.save
+      flash[:notice] = "Page was successfully created"
+      redirect_to :action => "list"
+    else
+      render_scaffold('new')
+    end
+  end
+
 end
