1963c712561d631b78479af6727ed2faab3d1373
simonlei <simonlei@36a1d1b6-861c-0410-8dcb-f754f489a112>
2006-09-04 15:12:32 +0000

    add the active rbac engine

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -1,5 +1,13 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
+require_dependency "#{RAILS_ROOT}/vendor/plugins/active_rbac/app/helpers/rbac_helper"
+
 class ApplicationController < ActionController::Base
+  helper RbacHelper
+
   layout "base"
+  
+  # The application controller will give us the "current_user" method.
+  include ActiveRbacMixins::ApplicationControllerMixin
+
 end
