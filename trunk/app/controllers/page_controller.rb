da26d41f571627511f7cab2c3cb5b9b2f4f919ac
simonlei <simonlei@36a1d1b6-861c-0410-8dcb-f754f489a112>
2006-09-05 13:52:29 +0000

    add the comment

--- trunk/app/controllers/page_controller.rb
+++ trunk/app/controllers/page_controller.rb
@@ -3,18 +3,25 @@ class PageController < ApplicationController
 
   def show
     @page = Page.find( params[:id])
+    @comments = @page.comments
+  end
+
+  def comment
+    comment = Comment.new( params[:comment])
+    comment.user_id = current_user.id
+    page = Page.find( params[:id]).add_comment comment
+    redirect_to :action => "show", :id => params[:id]
   end
 
   def new
     @page = Page.new
     @page.space_id = params[:space_id]
-#    @page.space = Space.find( params[:space_id])
     render_scaffold
   end
 
   def create
     @page = Page.new(params[:page])
-#    @page.space_id = params[:space_id]
+    @page.user_id = current_user.id
     if @page.save
       flash[:notice] = "Page was successfully created"
       redirect_to :action => "list"
