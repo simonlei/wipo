d4e635144b87b814d30c7019f18f649045a4fafd
simonlei <simonlei@36a1d1b6-861c-0410-8dcb-f754f489a112>
2006-09-29 10:02:25 +0000

    using utf-8 instead of gbk encoding

--- trunk/db/migrate/002_create_pages.rb
+++ trunk/db/migrate/002_create_pages.rb
@@ -5,10 +5,12 @@ class CreatePages < ActiveRecord::Migration
       t.column "content", :string
       t.column "creator_id", :integer
       t.column "user_id", :integer, :null => false
+      t.column "parent_id", :integer
       t.column "created_at", :datetime
       t.column "updated_at", :datetime
       t.column "space_id", :integer, :null => false
       t.column "type", :string
+      t.column "deleted", :boolean, :default => false
     end
   end
 
