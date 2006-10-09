3b493bad06a966523fe9ea35136ee01d50c6c487
simonlei <simonlei@36a1d1b6-861c-0410-8dcb-f754f489a112>
2006-10-09 15:25:20 +0000

    Add the url for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,14 @@
 module ApplicationHelper
   include WikiHelper
 
+  def url_for_user user
+    if user.personal_space_id > 0
+      link_to user.login, :controller=>"space", :action=>"show", :id => user.personal_space_id
+    else
+      user.login 
+    end
+  end
+
   def time_to_str time
     time.strftime "%Y 年 %m 月 %d 日 %H:%M" unless time.nil?
   end
