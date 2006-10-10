0dd186e7fcb4ad04459e3c9301d5b6f01fff0a23
simonlei <simonlei@36a1d1b6-861c-0410-8dcb-f754f489a112>
2006-10-10 14:29:36 +0000

    Add user link and css

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   include ActiveRbacMixins::ApplicationControllerMixin
 
   before_filter :set_charset 
-  before_filter :protect_controller, :except => [ :list, :index, :show, :welcome, :login, :download, :feed, :search, :displaypage]
+  before_filter :protect_controller, :except => [ :list, :index, :show, :welcome, :login, :download, :feed, :search, :displaypage, :show_home]
   
   def protect_controller 
     if current_user.has_role?("Admin")
